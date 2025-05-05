97221da4531d037baa3b05587660d33380c75569
Abdullah Asim <bscs23064@itu.edu.pk>
2025-05-05 16:48:44 +0500

    Initial commit: Barebone structure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Updated Content</h1>
+      <p>This should appear on the page!</p>
+    </div>
+  );
+}
+
+export default App;
